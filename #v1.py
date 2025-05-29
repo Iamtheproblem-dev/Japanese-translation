7286670837b02585264c26cbedf01388bdbd8026
Iamtheproblem-dev <jfkruczkowski@gmail.com>
2025-05-29 15:56:07 +0200

    Add files via upload

---
+++ #v1.py
@@ -0,0 +1,413 @@
+#v1
+
+char_map1 = {
+    'a': 'あ',
+    'e': 'え',
+    'i': 'い',
+    'u': 'う',
+    'o': 'お',
+    'A': 'あ',
+    'E': 'え',
+    'I': 'い',
+    'U': 'う',
+    'O': 'お',
+    'n': 'ん',
+    'N': 'ん',
+}
+    
+char_map2 = {
+    'ka': 'か',
+    'ki': 'き',
+    'ku': 'く',
+    'ke': 'け',
+    'ko': 'こ',
+    'Ka': 'か',
+    'Ki': 'き',
+    'Ku': 'く',
+    'Ke': 'け',
+    'Ko': 'こ',
+    'KA': 'か',
+    'KI': 'き',
+    'KU': 'く',
+    'KE': 'け',
+    'KO': 'こ',
+}
+
+char_map3 = {
+    'sa': 'さ',
+    'shi': 'し',
+    'su': 'す',
+    'se': 'せ',
+    'so': 'そ',
+    'Sa': 'さ',
+    'Shi': 'し',
+    'Su': 'す',
+    'Se': 'せ',
+    'So': 'そ',
+    'SA': 'さ',
+    'SHI': 'し',
+    'SU': 'す',
+    'SE': 'せ',
+    'SO': 'そ',
+}
+
+char_map4 = {
+    'ta': 'た',
+    'chi': 'ち',
+    'tsu': 'つ',
+    'te': 'て',
+    'to': 'と',
+    'Ta': 'た',
+    'Chi': 'ち',
+    'Tsu': 'つ',
+    'Te': 'て',
+    'To': 'と',
+    'TA': 'た',
+    'CHI': 'ち',
+    'TSU': 'つ',
+    'TE': 'て',
+    'TO': 'と',
+}
+
+char_map5 = {
+    'na': 'な',
+    'ni': 'に',
+    'nu': 'ぬ',
+    'ne': 'ね',
+    'no': 'の',
+    'Na': 'な',
+    'Ni': 'に',
+    'Nu': 'ぬ',
+    'Ne': 'ね',
+    'No': 'の',
+    'NA': 'な',
+    'NI': 'に',
+    'NU': 'ぬ',
+    'NE': 'ね',
+    'NO': 'の',
+}
+
+char_map6 = {
+    'ha': 'は',
+    'hi': 'ひ',
+    'fu': 'ふ',
+    'he': 'へ',
+    'ho': 'ほ',
+    'Ha': 'は',
+    'Hi': 'ひ',
+    'Fu': 'ふ',
+    'He': 'へ',
+    'Ho': 'ほ',
+    'HA': 'は',
+    'HI': 'ひ',
+    'FU': 'ふ',
+    'HE': 'へ',
+    'HO': 'ほ',
+}
+
+char_map7 = {
+    'ma': 'ま',
+    'mi': 'み',
+    'mu': 'む',
+    'me': 'め',
+    'mo': 'も',
+    'Ma': 'ま',
+    'Mi': 'み',
+    'Mu': 'む',
+    'Me': 'め',
+    'Mo': 'も',
+    'MA': 'ま',
+    'MI': 'み',
+    'MU': 'む',
+    'ME': 'め',
+    'MO': 'も',
+}
+
+char_map8 = {
+    'ra': 'ら',
+    'ri': 'り',
+    'ru': 'る',
+    're': 'れ',
+    'ro': 'ろ',
+    'Ra': 'ら',
+    'Ri': 'り',
+    'Ru': 'る',
+    'Re': 'れ',
+    'Ro': 'ろ',
+    'RA': 'ら',
+    'RI': 'り',
+    'RU': 'る',
+    'RE': 'れ',
+    'RO': 'ろ',
+}
+
+char_map9 = {
+    'wa': 'わ',
+    'wi' : 'ゐ',
+    'we': 'ゑ',
+    'wo': 'を',
+    'Wa': 'わ',
+    'Wi' : 'ゐ',
+    'We': 'ゑ',
+    'Wo': 'を',
+    'WA': 'わ',
+    'WI' : 'ゐ',
+    'WE': 'ゑ',
+    'WO': 'を',
+}
+
+char_map10 = {
+    'ya': 'や',
+    'yu': 'ゆ',
+    'yo': 'よ',
+    'Ya': 'や',
+    'Yu': 'ゆ',
+    'Yo': 'よ',
+    'YA': 'や',
+    'YU': 'ゆ',
+    'YO': 'よ',
+}
+
+char_map_short = {
+    'a': 'あ',
+    'A': 'あ',
+    'i': 'い',
+    'I': 'い',
+    'u': 'う',
+    'U': 'う',
+    'e': 'え',
+    'E': 'え',
+    'o': 'お',
+    'O': 'お',
+    'n': 'ん',
+    'N': 'ん',
+}
+
+
+
+char_map_long = {
+    'shi': 'し',
+    'Shi': 'し',
+    'SHI': 'し',
+    'chi': 'ち',
+    'Chi': 'ち',
+    'CHI': 'ち',
+    'tsu': 'つ',
+    'Tsu': 'つ',
+    'TSU': 'つ',
+}
+
+char_map_mid = {
+    'ka': 'か',
+    'Ka': 'か',
+    'KA': 'か',
+    'ki': 'き',
+    'Ki': 'き',
+    'KI': 'き',
+    'ku': 'く',
+    'Ku': 'く',
+    'KU': 'く',
+    'ke': 'け',
+    'Ke': 'け',
+    'KE': 'け',
+    'ko': 'こ',
+    'Ko': 'こ',
+    'KO': 'こ',
+    
+
+
+}
+char_map_all = {
+    'a': 'あ',
+    'e': 'え',
+    'i': 'い',
+    'u': 'う',
+    'o': 'お',
+    'A': 'あ',
+    'E': 'え',
+    'I': 'い',
+    'U': 'う',
+    'O': 'お',
+    'n': 'ん',
+    'N': 'ん',
+    'ka': 'か',
+    'ki': 'き',
+    'ku': 'く',
+    'ke': 'け',
+    'ko': 'こ',
+    'Ka': 'か',
+    'Ki': 'き',
+    'Ku': 'く',
+    'Ke': 'け',
+    'Ko': 'こ',
+    'KA': 'か',
+    'KI': 'き',
+    'KU': 'く',
+    'KE': 'け',
+    'KO': 'こ',
+    'sa': 'さ',
+    'shi': 'し',
+    'su': 'す',
+    'se': 'せ',
+    'so': 'そ',
+    'Sa': 'さ',
+    'Shi': 'し',
+    'Su': 'す',
+    'Se': 'せ',
+    'So': 'そ',
+    'SA': 'さ',
+    'SHI': 'し',
+    'SU': 'す',
+    'SE': 'せ',
+    'SO': 'そ',
+    'ta': 'た',
+    'chi': 'ち',
+    'tsu': 'つ',
+    'te': 'て',
+    'to': 'と',
+    'Ta': 'た',
+    'Chi': 'ち',
+    'Tsu': 'つ',
+    'Te': 'て',
+    'To': 'と',
+    'TA': 'た',
+    'CHI': 'ち',
+    'TSU': 'つ',
+    'TE': 'て',
+    'TO': 'と',
+    'na': 'な',
+    'ni': 'に',
+    'nu': 'ぬ',
+    'ne': 'ね',
+    'no': 'の',
+    'Na': 'な',
+    'Ni': 'に',
+    'Nu': 'ぬ',
+    'Ne': 'ね',
+    'No': 'の',
+    'NA': 'な',
+    'NI': 'に',
+    'NU': 'ぬ',
+    'NE': 'ね',
+    'NO': 'の',
+    'ha': 'は',
+    'hi': 'ひ',
+    'fu': 'ふ',
+    'he': 'へ',
+    'ho': 'ほ',
+    'Ha': 'は',
+    'Hi': 'ひ',
+    'Fu': 'ふ',
+    'He': 'へ',
+    'Ho': 'ほ',
+    'HA': 'は',
+    'HI': 'ひ',
+    'FU': 'ふ',
+    'HE': 'へ',
+    'HO': 'ほ',
+    'ma': 'ま',
+    'mi': 'み',
+    'mu': 'む',
+    'me': 'め',
+    'mo': 'も',
+    'Ma': 'ま',
+    'Mi': 'み',
+    'Mu': 'む',
+    'Me': 'め',
+    'Mo': 'も',
+    'MA': 'ま',
+    'MI': 'み',
+    'MU': 'む',
+    'ME': 'め',
+    'MO': 'も',
+    'ra': 'ら',
+    'ri': 'り',
+    'ru': 'る',
+    're': 'れ',
+    'ro': 'ろ',
+    'Ra': 'ら',
+    'Ri': 'り',
+    'Ru': 'る',
+    'Re': 'れ',
+    'Ro': 'ろ',
+    'RA': 'ら',
+    'RI': 'り',
+    'RU': 'る',
+    'RE': 'れ',
+    'RO': 'ろ',
+    'wa': 'わ',
+    'wi' : 'ゐ',
+    'we': 'ゑ',
+    'wo': 'を',
+    'Wa': 'わ',
+    'Wi' : 'ゐ',
+    'We': 'ゑ',
+    'Wo': 'を',
+    'WA': 'わ',
+    'WI' : 'ゐ',
+    'WE': 'ゑ',
+    'WO': 'を',
+    'ya': 'や',
+    'yu': 'ゆ',
+    'yo': 'よ',
+    'Ya': 'や',
+    'Yu': 'ゆ',
+    'Yo': 'よ',
+    'YA': 'や',
+    'YU': 'ゆ',
+    'YO': 'よ',
+}
+
+
+
+def convert_to_japanese_short(text):
+    return ''.join(char_map_short.get(char, char) for char in text)
+def convert_to_japanese_mid(text):
+    return ''.join(char_map_mid.get(char, char) for char in text)
+def convert_to_japanese_long(text):
+    return ''.join(char_map_long.get(char, char) for char in text)
+def main():
+    input_file_short = 'input_short.txt' 
+    output_file_short = 'output_short.txt'
+    input_file_mid = 'input_mid.txt'
+    output_file_mid = 'output_mid.txt'
+    input_file_long = 'input_long.txt'
+    output_file_long = 'output_long.txt'
+
+
+    try:
+        with open(input_file_short, 'r', encoding='utf-8') as f:
+            content_short = f.read()
+            converted_content_short = convert_to_japanese_short(content_short)
+
+        with open(output_file_short, 'w', encoding='utf-8-sig') as f:
+            f.write(converted_content_short)    
+            print(f"Conversion complete. Output written to {output_file_short}")
+
+    except FileNotFoundError:
+        try:
+            with open(input_file_mid, 'r', encoding= 'utf-8') as f:
+               content_mid = f.read()
+               converted_content_mid = convert_to_japanese_mid(content_mid)
+           
+            with open(output_file_mid, 'w', encoding='utf-8-sig') as f:
+               f.write(converted_content_mid)
+               print(f"Conversion complete. Output written to {output_file_mid}")
+      
+        except FileNotFoundError:
+            try:
+                with open(input_file_long, 'r', encoding='utf-8-sig') as f:
+                   content_long = f.read()
+                   converted_content_long = convert_to_japanese_long(content_long)
+
+                with open(output_file_long, 'w', encoding='utf-8-sig') as f:
+                   f.write(converted_content_long)
+                   print(f"Conversation complete. Output written to {output_file_long}")
+            
+            except FileNotFoundError:
+                print("COŚZJEBAŁEŚ")   
+
+
+
+if __name__ == '__main__':
+    main()
