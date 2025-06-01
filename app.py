e41409f24845f013bf160bab13a9f2be9be40400
Ignacy Grabowski <Igngrabowski@gmail.com>
2025-06-01 17:15:52 +0200

    Options on site

--- app.py
+++ app.py
@@ -1,21 +1,16 @@
-<<<<<<< HEAD
-from flask import Flask,render_template # type: ignore
-=======
+
 from flask import Flask,render_template,request,session
->>>>>>> caf9e8a807ec26e4ae3af14124eced721c166a75
 from os import urandom
 import default_gen
-
 app = Flask(__name__)
 app.config["SECRET_KEY"] = urandom(32)
 
 @app.route('/')
 def main():
-    #tutaj trzeba dac ten tekst
-    msg = 'hello world'
+    return render_template('html/main_page.html')
 
 
-    return render_template('html/main_page.html',media = msg)
+
 @app.route('/change', methods = ['POST'])
 def encode():
     msg = request.json
--- static/js/Text.js
+++ static/js/Text.js
@@ -1,15 +1,27 @@
 
 function upload(){
     const textarea = document.getElementById('input_text');
-    msg = JSON.stringify({ 'text': textarea.value })
+    var msg = new FormData
+
+    const radio = document.querySelector('input[name="option"]:checked');
+    if (radio == null){
+        let button = document.getElementById('upload')
+        button.innerHTML = 'select an option';
+
+    }else{
+
+       
+    msg['option'] = radio.id;  
+    msg['text'] = textarea.value;
+    
     fetch('/change', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: msg
+        body: JSON.stringify(msg)
     }).then(response => response.text())
     .then(response =>{
         textarea.value = response; 
-    })
+    })} 
 }
